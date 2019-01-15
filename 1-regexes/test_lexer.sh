b2dbc91078d84d08ba9381690690baa0376f39b7
John Wickerson <johnwickerson@cantab.net>
2019-01-15 14:38:29 +0000

    avoiding DOS2UNIX when in a Mac environment

--- 1-regexes/test_lexer.sh
+++ 1-regexes/test_lexer.sh
@@ -3,24 +3,32 @@
 # Try to find a portable way of getting rid of
 # any stray carriage returns
 if which dos2unix ; then
-    DOS2UNIX="dos2unix"
+    DOS2UNIX="| dos2unix"
 elif which fromdos ; then
-    DOS2UNIX="fromdos"
+    DOS2UNIX="| fromdos"
 else
-    # This works on a GNU version of sed. I think this
-    # will work in OSX as well, but don't have a machine
-    # on which to test that. From reading the OSX docs,
-    # it looks compatible.
-    # The code \x0D is the ASCII code of carriage-return,
-    # so it the regex should delete any CRs at the end of
-    # a line (or anywhere in a line)
-    DOS2UNIX="sed -e s/\x0D//g -"
-    # Tested for combinations of:
-    # - Ubuntu
-    # - Cygwin
-    # and inputs formats:
-    # - LF
-    # - CRLF
+    case "$(uname -s)" in
+    Darwin)
+        DOS2UNIX=""
+    ;;
+
+    *)
+        # This works on a GNU version of sed. I think this
+        # will work in OSX as well, but don't have a machine
+        # on which to test that. From reading the OSX docs,
+        # it looks compatible.
+        # The code \x0D is the ASCII code of carriage-return,
+        # so it the regex should delete any CRs at the end of
+        # a line (or anywhere in a line)
+        DOS2UNIX="| sed -e s/\x0D//g -"
+	# Tested for combinations of:
+	# - Ubuntu
+	# - Cygwin
+	# and inputs formats:
+	# - LF
+	# - CRLF
+    ;;
+    esac
 fi
 
 echo "========================================"
@@ -44,9 +52,9 @@ for i in test/in/*.txt; do
     echo ""
     echo "Input file : ${i}"
     BASENAME=$(basename $i .txt);
-    cat $i | ${DOS2UNIX} | ./histogram  > test/out/$BASENAME.stdout.txt  2> test/out/$BASENAME.stderr.txt
+    cat $i ${DOS2UNIX} | ./histogram  > test/out/$BASENAME.stdout.txt  2> test/out/$BASENAME.stderr.txt
 
-    diff <(cat test/ref/$BASENAME.stdout.txt | ${DOS2UNIX}) <(cat test/out/$BASENAME.stdout.txt) > test/out/$BASENAME.diff.txt
+    diff <(cat test/ref/$BASENAME.stdout.txt ${DOS2UNIX}) <(cat test/out/$BASENAME.stdout.txt) > test/out/$BASENAME.diff.txt
     if [[ "$?" -ne "0" ]]; then
         echo -e "\nERROR"
     else
@@ -59,6 +67,15 @@ done
 echo "########################################"
 echo "Passed ${PASSED} out of ${CHECKED}".
 echo ""
+
+case "$(uname -s)" in
+    Darwin)
+    echo ""
+    echo "Warning: This appears not to be a Linux environment"
+    echo "         Make sure you do a final run on a lab machine or an Ubuntu VM"
+    exit 0
+    ;;
+esac
 
 RELEASE=$(lsb_release -d)
 if [[ $? -ne 0 ]]; then
